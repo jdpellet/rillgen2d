bcb1d665d1bbe1d8b699717c55f0cae7be3a66f8
Jacob van der Leeuw <jvanderleeuw@email.arizona.edu>
2021-05-04 09:53:06 -0700

    testing

--- rillgen2d.py
+++ rillgen2d.py
@@ -1084,6 +1084,7 @@ class Application(tk.Frame):
         """Uses the map.html file to generate a folium map using QtWidgets.QWebEngineView()"""
         print("I AM REACHING HERE")
         print("path is:",Path.cwd().as_posix())
+        print("dir is:",os.getcwd())
         print(os.listdir())
         if os.path.exists("map.html"):
             mapfile = QtCore.QUrl.fromLocalFile(Path("map.html").resolve().as_posix())
